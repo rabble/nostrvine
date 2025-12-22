d7f655d5a0926898bd6c64d367acc6a923223557
John Weidner <john.weidner@verygood.ventures>
2025-12-22 17:33:12 -0500

    keycast_flutter package from Divine's Daniel Cadena

---
+++ mobile/packages/keycast_flutter/lib/keycast_flutter.dart
@@ -0,0 +1,22 @@
+// ABOUTME: Public API exports for keycast_flutter library
+// ABOUTME: OAuth authentication with PKCE and BYOK support for Nostr
+
+library keycast_flutter;
+
+export 'src/oauth/oauth_client.dart';
+export 'src/oauth/oauth_config.dart';
+export 'src/oauth/oauth_error.dart';
+export 'src/oauth/callback_result.dart';
+export 'src/oauth/token_response.dart';
+export 'src/oauth/pkce.dart';
+
+export 'src/rpc/keycast_rpc.dart';
+export 'src/rpc/rpc_response.dart';
+
+export 'src/models/keycast_session.dart';
+export 'src/models/exceptions.dart';
+
+export 'src/crypto/key_utils.dart';
+
+export 'src/storage/keycast_storage.dart';
+export 'src/storage/secure_storage.dart';
