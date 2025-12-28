bb78e48661de5fc46208621607296f0648d3cabf
John Weidner <john.weidner@verygood.ventures>
2025-12-28 13:56:41 -0600

    fix for text overflow in delete all content warning

--- mobile/lib/widgets/delete_account_dialog.dart
+++ mobile/lib/widgets/delete_account_dialog.dart
@@ -88,6 +88,7 @@ Future<void> showDeleteAllContentWarningDialog({
         'This is IRREVERSIBLE. Are you ABSOLUTELY CERTAIN?',
         style: TextStyle(color: Colors.white, fontSize: 16, height: 1.5),
       ),
+      scrollable: true,
       actions: [
         TextButton(
           onPressed: () => context.pop(),
