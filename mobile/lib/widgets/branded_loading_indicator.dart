9a7b512a33afc2891dc079ceaed33d7b7236d009
rabble <evan@protest.net>
2025-12-30 12:05:42 +1300

    feat: use animated GIF for branded loading indicator
    Switch to GIF format for the wings flapping animation since flutter_svg
    doesn't support SMIL animations. GIF is natively supported by Flutter's
    Image.asset() widget.
    
    Requires: assets/loading-brand.gif to be added

--- mobile/lib/widgets/branded_loading_indicator.dart
+++ mobile/lib/widgets/branded_loading_indicator.dart
@@ -1,55 +1,27 @@
-// ABOUTME: Branded loading indicator widget using the divine logo with rotation
+// ABOUTME: Branded loading indicator widget using the divine logo GIF animation
 // ABOUTME: Replaces generic CircularProgressIndicator with branded experience
 
 import 'package:flutter/material.dart';
 
 /// A branded loading indicator that displays the animated divine logo.
 ///
-/// This widget shows the divine play button logo with a smooth rotation
-/// animation to indicate loading state.
-class BrandedLoadingIndicator extends StatefulWidget {
+/// This widget shows the divine wings flapping animation as a GIF.
+class BrandedLoadingIndicator extends StatelessWidget {
   const BrandedLoadingIndicator({super.key, this.size = 80.0});
 
   /// The size (width and height) of the loading indicator.
   final double size;
 
   @override
-  State<BrandedLoadingIndicator> createState() =>
-      _BrandedLoadingIndicatorState();
-}
-
-class _BrandedLoadingIndicatorState extends State<BrandedLoadingIndicator>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 1500),
-      vsync: this,
-    )..repeat();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: widget.size,
-      height: widget.size,
-      child: RotationTransition(
-        turns: _controller,
-        child: Image.asset(
-          'assets/icon/divine_icon_transparent.png',
-          width: widget.size,
-          height: widget.size,
-          fit: BoxFit.contain,
-        ),
+      width: size,
+      height: size,
+      child: Image.asset(
+        'assets/loading-brand.gif',
+        width: size,
+        height: size,
+        fit: BoxFit.contain,
       ),
     );
   }
