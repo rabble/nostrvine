fd2ea1fdf55f5a07800e4b2b67745232402226af
Oscar Martin <martinm.oscar@gmail.com>
2025-12-30 10:32:57 +0100

    chore: update tests for new loading indicator

--- mobile/test/widgets/popular_videos_tab_test.dart
+++ mobile/test/widgets/popular_videos_tab_test.dart
@@ -12,6 +12,7 @@ import 'package:openvine/providers/video_events_providers.dart';
 import 'package:openvine/providers/app_providers.dart';
 import 'package:openvine/services/video_event_service.dart';
 import 'package:nostr_client/nostr_client.dart';
+import 'package:openvine/widgets/branded_loading_indicator.dart';
 import 'package:openvine/widgets/popular_videos_tab.dart';
 
 import '../test_data/video_test_data.dart';
@@ -104,7 +105,7 @@ void main() {
       );
       await tester.pump();
 
-      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      expect(find.byType(BrandedLoadingIndicator), findsOneWidget);
     });
 
     testWidgets('shows error message when loading fails', (tester) async {
