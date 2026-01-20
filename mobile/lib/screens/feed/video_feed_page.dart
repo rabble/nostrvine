a90be3cbf15aa2b516a7c74fa9db7d32b664a636
Oscar Martin <martinm.oscar@gmail.com>
2026-01-20 12:15:40 +0100

    refactor: move to its own class

--- mobile/lib/screens/feed/video_feed_page.dart
+++ mobile/lib/screens/feed/video_feed_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:openvine/blocs/video_feed/video_feed_bloc.dart';
 import 'package:openvine/providers/app_providers.dart';
+import 'package:openvine/screens/feed/feed_mode_switch.dart';
 import 'package:openvine/screens/feed/video_page_view.dart';
 import 'package:divine_ui/divine_ui.dart';
 import 'package:openvine/widgets/branded_loading_indicator.dart';
@@ -75,7 +75,7 @@ class _VideoFeedViewState extends State<VideoFeedView> {
                       )
                     : null,
               ),
-              const _FeedModeSwitch(),
+              const FeedModeSwitch(),
               // Loading more indicator
               if (state.isLoadingMore)
                 const Positioned(
@@ -94,107 +94,6 @@ class _VideoFeedViewState extends State<VideoFeedView> {
         },
       ),
     );
-  }
-}
-
-class _FeedModeSwitch extends StatelessWidget {
-  const _FeedModeSwitch();
-
-  static const _feedModeLabels = {
-    FeedMode.latest: 'New',
-    FeedMode.popular: 'Popular',
-    FeedMode.home: 'Following',
-  };
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      top: 0,
-      left: 0,
-      right: 0,
-      child: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0x3D000000), // rgba(0,0,0,0.24)
-              Colors.transparent,
-            ],
-          ),
-        ),
-        child: SafeArea(
-          bottom: false,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 6, bottom: 16),
-            child: BlocBuilder<VideoFeedBloc, VideoFeedState>(
-              buildWhen: (prev, curr) => prev.mode != curr.mode,
-              builder: (context, state) {
-                return Center(
-                  child: GestureDetector(
-                    onTap: () => _showFeedModeBottomSheet(context, state.mode),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          _feedModeLabels[state.mode] ?? state.mode.name,
-                          style: VineTheme.titleFont(fontSize: 28).copyWith(
-                            shadows: [
-                              const Shadow(
-                                color: Color(0x1A000000),
-                                offset: Offset(1, 1),
-                                blurRadius: 1,
-                              ),
-                              const Shadow(
-                                color: Color(0x1A000000),
-                                offset: Offset(0.4, 0.4),
-                                blurRadius: 0.6,
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        SizedBox(
-                          width: 32,
-                          height: 32,
-                          child: SvgPicture.asset(
-                            'assets/icon/CaretDown.svg',
-                            colorFilter: const ColorFilter.mode(
-                              Colors.white,
-                              BlendMode.srcIn,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Future<void> _showFeedModeBottomSheet(
-    BuildContext context,
-    FeedMode currentMode,
-  ) async {
-    final selected = await VineBottomSheetSelectionMenu.show(
-      context: context,
-      selectedValue: currentMode.name,
-      options: const [
-        VineBottomSheetSelectionOptionData(label: 'New', value: 'latest'),
-        VineBottomSheetSelectionOptionData(label: 'Popular', value: 'popular'),
-        VineBottomSheetSelectionOptionData(label: 'Following', value: 'home'),
-      ],
-    );
-
-    if (selected != null && context.mounted) {
-      final mode = FeedMode.values.firstWhere((m) => m.name == selected);
-      context.read<VideoFeedBloc>().add(VideoFeedModeChanged(mode));
-    }
   }
 }
 
