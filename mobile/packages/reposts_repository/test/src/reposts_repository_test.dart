bc6d3cca1ad3352aa0ee0214bc2771f056da30ef
b0berman <hugo@verygood.ventures>
2026-01-23 14:10:32 +0100

    chore: rm useless test

--- mobile/packages/reposts_repository/test/src/reposts_repository_test.dart
+++ mobile/packages/reposts_repository/test/src/reposts_repository_test.dart
@@ -995,27 +995,4 @@ void main() {
       });
     });
   });
-
-  group('buildAddressableId', () {
-    test('builds correct addressable ID format', () {
-      final addressableId = AId(
-        kind: EventKind.videoVertical,
-        pubkey: 'abc123',
-        dTag: 'my-video',
-      );
-      expect(addressableId, equals('34236:abc123:my-video'));
-    });
-
-    test('handles special characters in d-tag', () {
-      final addressableId = AId(
-        kind: EventKind.videoVertical,
-        pubkey: 'pubkey',
-        dTag: 'video-with-dashes_and_underscores',
-      );
-      expect(
-        addressableId,
-        equals('34236:pubkey:video-with-dashes_and_underscores'),
-      );
-    });
-  });
 }
