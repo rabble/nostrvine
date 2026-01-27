484409182db885967803efeb2e7ec764d069fa58
hm21 <alex.frei@hotmail.ch>
2026-01-27 19:40:49 +0100

    fix(camera): add test for startRecording handling PlatformException

--- mobile/test/widgets/video_editor/video_editor_bottom_bar_test.dart
+++ mobile/test/widgets/video_editor/video_editor_bottom_bar_test.dart
@@ -4,11 +4,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:openvine/constants/video_editor_constants.dart';
 import 'package:openvine/models/clip_manager_state.dart';
 import 'package:openvine/models/recording_clip.dart';
 import 'package:openvine/models/video_editor/video_editor_provider_state.dart';
 import 'package:openvine/providers/clip_manager_provider.dart';
 import 'package:openvine/providers/video_editor_provider.dart';
+import 'package:openvine/utils/video_editor_utils.dart';
 import 'package:openvine/widgets/video_editor/video_editor_bottom_bar.dart';
 import 'package:openvine/widgets/video_editor/video_time_display.dart';
 import 'package:pro_video_editor/pro_video_editor.dart';
@@ -89,13 +91,29 @@ void main() {
 
     testWidgets('displays time display', (tester) async {
       await tester.pumpWidget(
-        buildTestWidget(totalDuration: const Duration(seconds: 30)),
+        buildTestWidget(totalDuration: const Duration(seconds: 3)),
       );
       await tester.pump();
 
       // VideoTimeDisplay should be present with correct duration
       expect(find.byType(VideoTimeDisplay), findsOneWidget);
-      expect(find.textContaining('30.00s'), findsOneWidget);
+      expect(find.textContaining('3.00s'), findsOneWidget);
+    });
+
+    testWidgets('limit time display to maximum', (tester) async {
+      await tester.pumpWidget(
+        buildTestWidget(totalDuration: VideoEditorConstants.maxDuration * 2),
+      );
+      await tester.pump();
+
+      // VideoTimeDisplay should be present with correct duration
+      expect(find.byType(VideoTimeDisplay), findsOneWidget);
+      expect(
+        find.textContaining(
+          VideoEditorConstants.maxDuration.toFormattedSeconds(),
+        ),
+        findsOneWidget,
+      );
     });
 
     testWidgets('play button is tappable', (tester) async {
