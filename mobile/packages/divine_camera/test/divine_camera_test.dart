7046f2a7c8d25e03f0419ad02e9d7efcefc90519
hm21 <alex.frei@hotmail.ch>
2026-02-15 11:03:39 +0100

    test: Update expected props length in camera state test

--- mobile/packages/divine_camera/test/divine_camera_test.dart
+++ mobile/packages/divine_camera/test/divine_camera_test.dart
@@ -637,7 +637,7 @@ void main() {
 
       final props = state.props;
 
-      expect(props.length, 15);
+      expect(props.length, 16);
       expect(props[0], isTrue); // isInitialized
       expect(props[1], isFalse); // isRecording
       expect(props[4], DivineCameraLens.back); // lens
