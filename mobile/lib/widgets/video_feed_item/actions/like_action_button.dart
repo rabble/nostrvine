c3af4f128b6ed70009de7ad658ab70acdc64f87c
hm21 <alex.frei@hotmail.ch>
2026-02-20 18:06:13 +0100

    fix: resolve issue that metascreen video-preview is broken

--- mobile/lib/widgets/video_feed_item/actions/like_action_button.dart
+++ mobile/lib/widgets/video_feed_item/actions/like_action_button.dart
@@ -15,30 +15,58 @@ import 'package:openvine/widgets/video_feed_item/actions/video_action_button.dar
 ///
 /// Requires [VideoInteractionsBloc] to be provided in the widget tree.
 class LikeActionButton extends StatelessWidget {
-  const LikeActionButton({required this.video, super.key});
+  const LikeActionButton({
+    required this.video,
+    super.key,
+    this.isPreviewMode = false,
+  });
 
   final VideoEvent video;
+  final bool isPreviewMode;
 
   @override
   Widget build(BuildContext context) {
+    if (isPreviewMode) return const _ActionButton();
+
     return BlocBuilder<VideoInteractionsBloc, VideoInteractionsState>(
       builder: (context, state) {
         final isLiked = state.isLiked;
         final likeCount = state.likeCount ?? 0;
         final totalLikes = likeCount + (video.originalLikes ?? 0);
 
-        return VideoActionButton(
-          iconAsset: 'assets/icon/content-controls/like.svg',
-          semanticIdentifier: 'like_button',
-          semanticLabel: isLiked ? 'Unlike video' : 'Like video',
-          iconColor: isLiked ? VineTheme.likeRed : VineTheme.whiteText,
-          isLoading: state.isLikeInProgress,
-          count: totalLikes,
-          onPressed: () {
-            context.read<VideoInteractionsBloc>().add(
-              const VideoInteractionsLikeToggled(),
-            );
-          },
+        return _ActionButton(
+          isLiked: isLiked,
+          isLikeInProgress: state.isLikeInProgress,
+          totalLikes: totalLikes,
+        );
+      },
+    );
+  }
+}
+
+class _ActionButton extends StatelessWidget {
+  const _ActionButton({
+    this.isLiked = false,
+    this.isLikeInProgress = false,
+    this.totalLikes = 1,
+  });
+
+  final bool isLiked;
+  final bool isLikeInProgress;
+  final int totalLikes;
+
+  @override
+  Widget build(BuildContext context) {
+    return VideoActionButton(
+      iconAsset: 'assets/icon/content-controls/like.svg',
+      semanticIdentifier: 'like_button',
+      semanticLabel: isLiked ? 'Unlike video' : 'Like video',
+      iconColor: isLiked ? VineTheme.likeRed : VineTheme.whiteText,
+      isLoading: isLikeInProgress,
+      count: totalLikes,
+      onPressed: () {
+        context.read<VideoInteractionsBloc>().add(
+          const VideoInteractionsLikeToggled(),
         );
       },
     );
