4562a580d8d6f0b81105b1c0a119048ead53cb69
hm21 <alex.frei@hotmail.ch>
2026-02-24 12:53:45 +0100

    feat: implement save to gallery functionality in video metadata bottom bar

--- mobile/lib/providers/video_recorder_provider.dart
+++ mobile/lib/providers/video_recorder_provider.dart
@@ -12,7 +12,6 @@ import 'package:openvine/models/audio_event.dart';
 import 'package:openvine/models/video_recorder/video_recorder_flash_mode.dart';
 import 'package:openvine/models/video_recorder/video_recorder_provider_state.dart';
 import 'package:openvine/models/video_recorder/video_recorder_timer_duration.dart';
-import 'package:openvine/providers/app_providers.dart';
 import 'package:openvine/providers/clip_manager_provider.dart';
 import 'package:openvine/providers/sounds_providers.dart';
 import 'package:openvine/screens/home_screen_router.dart';
@@ -683,17 +682,6 @@ class VideoRecorderNotifier extends Notifier<VideoRecorderProviderState> {
       '📊 Video duration: ${metadata.duration.inMilliseconds}ms',
       name: 'VideoRecorderNotifier',
       category: .video,
-    );
-
-    // Save clip to device gallery (fire-and-forget)
-    unawaited(
-      ref
-          .read(gallerySaveServiceProvider)
-          .saveVideoToGallery(
-            videoResult,
-            aspectRatio: state.aspectRatio,
-            metadata: metadata,
-          ),
     );
 
     // Generate and attach thumbnail.
