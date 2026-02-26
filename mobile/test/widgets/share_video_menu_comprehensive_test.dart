c90aef17aa52411281c7c17f2c8e3937004ce603
Meylis Annagurbanov <meylis.annagurbanow98@gmail.com>
2026-02-26 17:45:36 +0500

    fix: use sync bookmark service override to prevent null in initState
    The bookmarkServiceProvider.overrideWith used `async =>` which caused
    the AsyncValue to start as AsyncLoading. When _UnifiedShareSheetState
    .initState reads `.value`, it got null, making the BLoC's save handler
    silently skip the bookmark call and emit SaveFailure instead of
    SaveSuccess.

--- mobile/test/widgets/share_video_menu_comprehensive_test.dart
+++ mobile/test/widgets/share_video_menu_comprehensive_test.dart
@@ -84,9 +84,7 @@ void main() {
     }) => testProviderScope(
       mockUserProfileService: createMockUserProfileService(),
       additionalOverrides: [
-        bookmarkServiceProvider.overrideWith(
-          (ref) async => mockBookmarkService,
-        ),
+        bookmarkServiceProvider.overrideWith((ref) => mockBookmarkService),
         videoSharingServiceProvider.overrideWith(
           (ref) => mockVideoSharingService,
         ),
@@ -311,9 +309,7 @@ void main() {
         mockUserProfileService: createMockUserProfileService(),
         additionalOverrides: [
           followRepositoryProvider.overrideWithValue(null),
-          bookmarkServiceProvider.overrideWith(
-            (ref) async => mockBookmarkService,
-          ),
+          bookmarkServiceProvider.overrideWith((ref) => mockBookmarkService),
           videoSharingServiceProvider.overrideWith(
             (ref) => mockVideoSharingService,
           ),
