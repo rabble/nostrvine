7dcaeda49572ae4318e0749a70756090120fc212
hm21 <alex.frei@hotmail.ch>
2026-03-10 18:55:47 +0100

    refactor: remove unused google_fonts import from video_editor_gallery_instruction_text_test

--- mobile/test/widgets/video_clip_editor/gallery/video_editor_gallery_instruction_text_test.dart
+++ mobile/test/widgets/video_clip_editor/gallery/video_editor_gallery_instruction_text_test.dart
@@ -4,7 +4,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:openvine/blocs/video_editor/clip_editor/clip_editor_bloc.dart';
 import 'package:openvine/widgets/video_editor/clip_editor/gallery/video_editor_gallery_instruction_text.dart';
 
