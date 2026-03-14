69743cc5be9252434b553d2c6cf128479b1bd333
hm21 <alex.frei@hotmail.ch>
2026-03-14 08:12:41 +0100

    refactor(video-editor): remove unused import for listEquals in video editor screen

--- mobile/lib/screens/video_editor/video_editor_screen.dart
+++ mobile/lib/screens/video_editor/video_editor_screen.dart
@@ -5,7 +5,6 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:divine_ui/divine_ui.dart';
-import 'package:flutter/foundation.dart' show listEquals;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
