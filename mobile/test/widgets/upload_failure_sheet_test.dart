3de95cf90817ff64dc69d60001b70e7b8437c12e
hm21 <alex.frei@hotmail.ch>
2026-03-15 13:33:23 +0100

    feat: add tests for updating publish status and handling errors in video drafts

--- mobile/test/widgets/upload_failure_sheet_test.dart
+++ mobile/test/widgets/upload_failure_sheet_test.dart
@@ -2,7 +2,6 @@
 // ABOUTME: Verifies retry, save-to-drafts actions and UI rendering.
 
 import 'package:bloc_test/bloc_test.dart';
-import 'package:divine_ui/divine_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
