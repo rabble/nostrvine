68cefca3becbd6181447b54c5d21f788604824e1
rabble-claw <263104957+rabble-claw@users.noreply.github.com>
2026-03-16 08:50:06 +1300

    test: fix const lint in notifications navigation test

--- mobile/test/screens/notifications_navigation_test.dart
+++ mobile/test/screens/notifications_navigation_test.dart
@@ -49,7 +49,7 @@ void main() {
         videoEventServiceProvider.overrideWithValue(videoService),
         nostrServiceProvider.overrideWithValue(nostrClient),
       ],
-      child: MaterialApp(
+      child: const MaterialApp(
         home: Scaffold(body: NotificationsScreen()),
       ),
     );
