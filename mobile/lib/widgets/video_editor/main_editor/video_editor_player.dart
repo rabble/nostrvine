7d143732db7c62cd47087efd855eaf20189e6299
rabble <evan@protest.net>
2026-04-26 11:59:55 -0500

    fix(video-editor): preserve square preview corners

--- mobile/lib/widgets/video_editor/main_editor/video_editor_player.dart
+++ mobile/lib/widgets/video_editor/main_editor/video_editor_player.dart
@@ -28,7 +28,7 @@ class VideoEditorPlayer extends StatelessWidget {
       clipper: _RoundedRectClipper(
         bodySize: bodySize,
         targetAspectRatio: targetAspectRatio.value,
-        borderRadius: VideoEditorConstants.canvasRadius,
+        borderRadius: canvasBorderRadiusForAspectRatio(targetAspectRatio),
       ),
       child: AspectRatio(
         aspectRatio: aspectRatio,
@@ -39,6 +39,13 @@ class VideoEditorPlayer extends StatelessWidget {
       ),
     );
   }
+}
+
+@visibleForTesting
+double canvasBorderRadiusForAspectRatio(model.AspectRatio targetAspectRatio) {
+  return targetAspectRatio == model.AspectRatio.square
+      ? 0
+      : VideoEditorConstants.canvasRadius;
 }
 
 class _RoundedRectClipper extends CustomClipper<Path> {
