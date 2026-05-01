48c8bfe48eecdc6b51fcef4aa0a3228ab458a34d
hm21 <alex.frei@waio.ch>
2026-05-01 16:17:57 +0200

    fix(video): format subtitleLayer assignment for better readability

--- mobile/lib/widgets/video_feed_item/feed_videos.dart
+++ mobile/lib/widgets/video_feed_item/feed_videos.dart
@@ -472,8 +472,8 @@ class __OverlayState extends ConsumerState<_Overlay> {
                       isAutoEnabled: effectiveAutoEnabled,
                       onAutoPressed: widget.onToggleAutoAdvance,
                       onInteracted: widget.onSuppressAutoAdvance,
-                      subtitleLayer: video.hasSubtitles &&
-                              widget.controller != null
+                      subtitleLayer:
+                          video.hasSubtitles && widget.controller != null
                           ? _SubtitleLayer(
                               video: video,
                               controller: widget.controller!,
