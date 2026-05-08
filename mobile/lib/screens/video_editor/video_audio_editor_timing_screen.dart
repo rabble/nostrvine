73123dfd293a273cd5f56009547e51331df122d4
Liz Sweigart <127434495+NotThatKindOfDrLiz@users.noreply.github.com>
2026-05-07 23:02:51 -0500

    test(video-editor): inject clip player in timing screen tests

--- mobile/lib/screens/video_editor/video_audio_editor_timing_screen.dart
+++ mobile/lib/screens/video_editor/video_audio_editor_timing_screen.dart
@@ -17,6 +17,7 @@ import 'package:openvine/l10n/l10n.dart';
 import 'package:openvine/widgets/stereo_waveform_painter.dart';
 import 'package:openvine/widgets/video_editor/audio_editor/video_editor_audio_chip.dart';
 import 'package:openvine/widgets/video_editor/video_editor_toolbar.dart';
+import 'package:sound_service/sound_service.dart';
 
 /// Result of the audio timing screen.
 ///
@@ -55,6 +56,7 @@ class VideoAudioEditorTimingScreen extends StatefulWidget {
   const VideoAudioEditorTimingScreen({
     required this.sound,
     this.enableDeleteButton = true,
+    this.clipPlayer,
     super.key,
   });
 
@@ -63,6 +65,9 @@ class VideoAudioEditorTimingScreen extends StatefulWidget {
 
   /// Whether the delete button is shown in the toolbar.
   final bool enableDeleteButton;
+
+  /// Optional audio clip player override for tests.
+  final AudioClipPlayer? clipPlayer;
 
   /// Route name for navigation.
   static const routeName = 'video-audio-timing';
@@ -91,7 +96,10 @@ class _VideoAudioEditorTimingScreenState
     _waveformBloc = SoundWaveformBloc();
     _flingController = AnimationController.unbounded(vsync: this);
     _flingController.addListener(_onFlingUpdate);
-    _audioTimingCubit = AudioTimingCubit(sound: widget.sound);
+    _audioTimingCubit = AudioTimingCubit(
+      sound: widget.sound,
+      clipPlayer: widget.clipPlayer,
+    );
 
     // Delay initialization until after first frame
     WidgetsBinding.instance.addPostFrameCallback((_) {
