9acfb66222e0a0b5c9f117694f1b4dc1a0cb0b3e
rabble <evan@protest.net>
2026-05-08 10:00:51 +0300

    fix(hashtags): repair rebase localization wiring

--- mobile/lib/screens/search_results/widgets/videos_section.dart
+++ mobile/lib/screens/search_results/widgets/videos_section.dart
@@ -11,7 +11,6 @@ import 'package:openvine/blocs/search_results_filter/search_results_filter.dart'
 import 'package:openvine/blocs/video_search/video_search_bloc.dart';
 import 'package:openvine/l10n/l10n.dart';
 import 'package:openvine/providers/app_providers.dart';
-import 'package:openvine/l10n/search_results_filter_l10n.dart';
 import 'package:openvine/screens/feed/pooled_fullscreen_video_feed_screen.dart';
 import 'package:openvine/screens/search_results/widgets/search_section_empty_state.dart';
 import 'package:openvine/screens/search_results/widgets/search_section_error_state.dart';
