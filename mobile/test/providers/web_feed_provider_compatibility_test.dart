c0d4e127f43a2a6585459d6457e30150b92a117e
Liz Sweigart <127434495+NotThatKindOfDrLiz@users.noreply.github.com>
2026-05-13 15:17:47 -0500

    test(providers): isolate web feed compatibility storage deps

--- mobile/test/providers/web_feed_provider_compatibility_test.dart
+++ mobile/test/providers/web_feed_provider_compatibility_test.dart
@@ -1,13 +1,29 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:nostr_key_manager/nostr_key_manager.dart';
 import 'package:openvine/features/feature_flags/providers/feature_flag_providers.dart';
 import 'package:openvine/providers/app_providers.dart';
 import 'package:openvine/providers/shared_preferences_provider.dart';
 import 'package:openvine/services/openvine_media_cache.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../helpers/test_helpers.dart' show MockSecureStorage;
+import '../test_setup.dart';
+
+class _MockSecureKeyStorage extends Mock implements SecureKeyStorage {}
+
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+  setupTestEnvironment();
+
+  setUpAll(() {
+    registerFallbackValue(
+      SecureKeyContainer.fromNsec(
+        'nsec1vl029mgpspedva04g90vltkh6fvh240zqtv9k0t9af8935ke9laqsnlfe5',
+      ),
+    );
+  });
 
   setUp(() async {
     SharedPreferences.setMockInitialValues({});
@@ -15,8 +31,37 @@ void main() {
 
   test('pooled feed provider chain is web-compatible', () async {
     final prefs = await SharedPreferences.getInstance();
+    final mockKeyStorage = _MockSecureKeyStorage();
+    final secureStorage = MockSecureStorage();
+
+    when(() => mockKeyStorage.initialize()).thenAnswer((_) async {});
+    when(() => mockKeyStorage.hasKeys()).thenAnswer((_) async => false);
+    when(() => mockKeyStorage.getKeyContainer()).thenAnswer((_) async => null);
+    when(() => mockKeyStorage.clearCache()).thenReturn(null);
+    when(() => mockKeyStorage.dispose()).thenReturn(null);
+    when(() => mockKeyStorage.deleteKeys()).thenAnswer((_) async {});
+    when(
+      () => mockKeyStorage.storeIdentityKeyContainer(any(), any()),
+    ).thenAnswer((_) async {});
+    when(
+      () => mockKeyStorage.getIdentityKeyContainer(
+        any(),
+        biometricPrompt: any(named: 'biometricPrompt'),
+      ),
+    ).thenAnswer((_) async => null);
+    when(
+      () => mockKeyStorage.switchToIdentity(
+        any(),
+        biometricPrompt: any(named: 'biometricPrompt'),
+      ),
+    ).thenAnswer((_) async => true);
+
     final container = ProviderContainer(
-      overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(prefs),
+        secureKeyStorageProvider.overrideWith((ref) => mockKeyStorage),
+        flutterSecureStorageProvider.overrideWith((ref) => secureStorage),
+      ],
     );
     addTearDown(container.dispose);
 
