b17c4f497f02337e870057502929efd932d21863
hm21 <alex.frei@waio.ch>
2026-05-26 14:31:58 +0700

    refactor(video-editor): remove unused ClipReverseResultListener widget

--- mobile/lib/widgets/video_editor/video_editor_scaffold.dart
+++ mobile/lib/widgets/video_editor/video_editor_scaffold.dart
@@ -7,7 +7,6 @@ import 'package:openvine/blocs/video_editor/clip_editor/clip_editor_bloc.dart';
 import 'package:openvine/blocs/video_editor/main_editor/video_editor_main_bloc.dart';
 import 'package:openvine/blocs/video_editor/timeline_overlay/timeline_overlay_bloc.dart';
 import 'package:openvine/constants/video_editor_constants.dart';
-import 'package:openvine/extensions/video_editor_extensions.dart';
 import 'package:openvine/extensions/video_editor_history_extensions.dart';
 import 'package:openvine/l10n/l10n.dart';
 import 'package:openvine/widgets/branded_loading_scaffold.dart';
@@ -47,10 +46,8 @@ class VideoEditorScaffold extends StatelessWidget {
         resizeToAvoidBottomInset: false,
         floatingActionButton: const _AddElementFab(),
         body: _SplitFailureListener(
-          child: _ClipReverseResultListener(
-            child: _AudioExtractionResultListener(
-              child: _ScaffoldBody(isLoading: isLoading),
-            ),
+          child: _AudioExtractionResultListener(
+            child: _ScaffoldBody(isLoading: isLoading),
           ),
         ),
       ),
@@ -118,29 +115,6 @@ class _SplitFailureListener extends StatelessWidget {
       },
       child: child,
     );
-  }
-}
-
-class _ClipReverseResultListener extends StatelessWidget {
-  const _ClipReverseResultListener({required this.child});
-
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocListener<ClipEditorBloc, ClipEditorState>(
-      listenWhen: (prev, curr) =>
-          !identical(prev.lastReverseResult, curr.lastReverseResult) &&
-          curr.lastReverseResult != null,
-      listener: _onReverseResult,
-      child: child,
-    );
-  }
-
-  void _onReverseResult(BuildContext context, ClipEditorState state) {
-    if (state.lastReverseResult case ClipReverseSuccess()) {
-      VideoEditorScope.of(context).requireEditor.setClipState(state.clips);
-    }
   }
 }
 
