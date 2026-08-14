010b6c6a48e8fd3dfb02a5df28276d21f7b509f3
Daniel Cadenas <dcadenas@gmail.com>
2026-08-14 02:17:25 -0300

    fix(nostr): reconcile signer exports with main

--- mobile/packages/nostr_sdk/lib/nostr_sdk.dart
+++ mobile/packages/nostr_sdk/lib/nostr_sdk.dart
@@ -63,8 +63,8 @@ export 'signer/local_nostr_signer.dart';
 // Signing implementations
 export 'signer/isolate_decrypt_signer.dart';
 export 'signer/nostr_signer.dart';
-export 'signer/pubkey_only_nostr_signer.dart';
 export 'signer/signer_failure.dart';
+export 'signer/unauthenticated_signer.dart';
 export 'subscription.dart';
 export 'upload/blossom_uploader.dart';
 export 'upload/nip96_uploader.dart';
